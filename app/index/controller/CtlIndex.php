1206ce101b1eadf968af9db778dfe13f279e5b66
lwj0630 <965705672@qq.com>
2022-01-21 18:36:47 +0800

    1

--- app/index/controller/CtlIndex.php
+++ app/index/controller/CtlIndex.php
@@ -13,40 +13,7 @@ class CtlIndex
 
     public function index()
     {
-        $articleList = $this->mod->articleList();
-        $out = [
-            'list' => $articleList,
-        ];
-        return view('/index', $out);
-    }
-
-    public function details(Request $request)
-    {
-        $article_id = $request->param('id');
-        $details = $this->mod->articleDetails($article_id);
-        $other = $this->mod->articleOther($article_id);
-        $comment = $this->mod->articleComment($article_id);
-        $out = [
-            'data' => $details,
-            'other' => $other,
-            'comment' => $comment,
-        ];
-        return view('/details', $out);
-    }
-
-    public function commentPublish(Request $request)
-    {
-        $params = [
-            'article_id' => $request->param('article_id'),
-            'content' => $request->param('content'),
-            'atime' => time(),
-        ];
-        $ret = $this->mod->commentPublish($params);
-        if($ret) {
-            return $this->success('评论成功');
-        } else {
-            return $this->error('评论失败');
-        }
+        return view('/index');
     }
 
 }
--- app/index/service/SrvIndex.php
+++ app/index/service/SrvIndex.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\service;
 
-class Index
+class SrvIndex
 {
 
 }
